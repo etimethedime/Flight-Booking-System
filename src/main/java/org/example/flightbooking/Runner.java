46c6bc300dbeb9b86d2022b286a9e43fe9a0c7a0
mikiyasmekasha <mikiyasmekasha12@gmail.com>
2024-11-20 14:38:05 -0500

    Miki added a new

--- src/main/java/org/example/flightbooking/Runner.java
+++ src/main/java/org/example/flightbooking/Runner.java
@@ -3,7 +3,7 @@ package org.example.flightbooking;
 public class Runner {
     public static void main(String[] args) {
         System.out.println("Cosmas Ready");
-        System.out.println("I fucking hate it here");
+        System.out.println("I fucking hate it here fuck this class");
         System.out.println("EZRA ACTIVE");
     }
 
