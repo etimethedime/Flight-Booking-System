928b93e5adc492ddf35343b88c659fc86c3968d2
mikiyasmekasha <mikiyasmekasha12@gmail.com>
2024-11-20 13:47:56 -0500

    Miki added Flightbooker

---
+++ src/main/java/org/example/flightbooking/FlightBooker.java
@@ -0,0 +1,17 @@
+package org.example.flightbooking;
+
+import java.util.ArrayList;
+
+public class FlightBooker {
+    private ArrayList flightsAvailable;
+    private ArrayList flightsBooked;
+    private ArrayList seatsAvailable;
+    private ArrayList seatsBooked;
+    private ArrayList accommodations;
+
+    public FlightBooker(){
+
+    }
+
+}
+
