f350d9a3e56ddbfd8df7d28c573dd25f8bd4c3a2
cosmasobi <cosmasobi67@gmail.com>
2024-11-20 14:32:43 -0500

    COSMAS READY

--- src/main/java/org/example/flightbooking/Runner.java
+++ src/main/java/org/example/flightbooking/Runner.java
@@ -2,7 +2,7 @@ package org.example.flightbooking;
 
 public class Runner {
     public static void main(String[] args) {
-        System.out.println("Cosmas Ready");
+        System.out.println("COSMAS READY");
         System.out.println(" MIKI READY");
         System.out.println("EZRA ACTIVE");
     }
