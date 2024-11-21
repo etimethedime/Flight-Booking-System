ff42ab4bb2757f37cbf364b1a39b5028b5921101
ezra5 <ezra54311@gmail.com>
2024-11-21 18:20:36 -0500

    runner started

--- src/main/java/org/example/flightbooking/Runner.java
+++ src/main/java/org/example/flightbooking/Runner.java
@@ -1,8 +1,13 @@
 package org.example.flightbooking;
 
+import java.util.Scanner;
+
 public class Runner {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Username: ");
+        System.out.println("Enter a password: ");
+        //Customer c1 = new Customer();
     }
 
 }
