644627b35f0a11b1b47ddef8bf04c6600b5a695a
ezra5 <ezra54311@gmail.com>
2024-11-21 18:09:08 -0500

    readability

--- src/main/java/org/example/flightbooking/Customer.java
+++ src/main/java/org/example/flightbooking/Customer.java
@@ -1,9 +1,15 @@
 package org.example.flightbooking;
 
 public class Customer extends Account {
-    public Customer (String firstName, String lastName, String street, String zipCode, String state, String username,
-                    String password, String email, long SSN, String secQuestion, String secAnswer, String currentFlight, int secPIN) {
-        super(firstName, lastName, street, zipCode, state, username, password, email, SSN, secQuestion, secAnswer, currentFlight, secPIN);
+    public Customer (String firstName, String lastName, String street,
+                     String zipCode, String state, String username,
+                    String password, String email, long SSN,
+                     String secQuestion, String secAnswer, String currentFlight, int secPIN) {
+
+        super(firstName, lastName, street,
+                zipCode, state, username,
+                password, email, SSN,
+                secQuestion, secAnswer, currentFlight, secPIN);
     }
 
 }
