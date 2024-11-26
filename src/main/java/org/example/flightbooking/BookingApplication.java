c3e133e2c93567a9bec6ed60b893d8f1f9be3c78
ezra5 <ezra54311@gmail.com>
2024-11-25 23:50:08 -0500

    OG Booking App

--- src/main/java/org/example/flightbooking/BookingApplication.java
+++ src/main/java/org/example/flightbooking/BookingApplication.java
@@ -1,11 +1,9 @@
 package org.example.flightbooking;
 
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -13,39 +11,45 @@ import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+
 public class BookingApplication extends Application{
     //Buttons
-    @FXML
-    private Button registerButton;
-    @FXML
-    private Button logInButton;
-    @FXML
-    private Button leaveRegister;
-    @FXML
-    private Button leaveLogIn;
+    Button registerButton;
+    Button logInButton;
+    Button leaveRegister;
+    Button leaveLogIn;
+
 
     //Stages
     Stage window;
 
+
     //Scenes
     Scene openScene, logInScene, registerScene;
 
+
     @Override
     public void start(Stage window) throws Exception {
+
 
         //The Stage name
         window.setTitle("Flight Booker");
 
 
+
+
         //Labels are static non-interactive text!
         Label l1 = new Label("Welcome to CME Bookings!");
+
 
         //log in
         Label l3 = new Label("Welcome Back!");
         //register
         Label l2 = new Label("New Customer!");
 
-                //Giving Buttons a name and an Action
+
+        //Giving Buttons a name and an Action
+
 
         //giving them a name
         registerButton = new Button();
@@ -55,13 +59,16 @@ public class BookingApplication extends Application{
             @Override
             public void handle(ActionEvent actionEvent) {
 
+
                 //performing action when button is clicked
                 if(actionEvent.getSource() == registerButton){
                     window.setScene(registerScene);
                 }
 
+
             }
         });
+
 
         leaveRegister = new Button();
         leaveRegister.setText("Back");
@@ -74,46 +81,59 @@ public class BookingApplication extends Application{
             }
         });
 
+
         //simple version using Lambda Expression version (USE THIS TECHNIQUE)
         logInButton = new Button("Log In"); // you can set text in the constructor parameter
         logInButton.setOnAction(e -> window.setScene(logInScene));//you can set action with (e -> action)
+
 
         leaveLogIn = new Button("Back");
         leaveLogIn.setOnAction(e -> window.setScene(openScene));
 
 
-                                                                                //Layouts are a way to organize the app
 
 
-                // Layout 1 = VBOX - sets children in a vertical stack
+        //Layouts are a way to organize the app
+
+
+
+
+        // Layout 1 = VBOX - sets children in a vertical stack
+
 
         //the open scene
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/flightbooking/openScene.fxml"));
-        Parent root = loader.load();
-        openScene = new Scene(root, 400, 400);
+        VBox vboxOpenScene = new VBox(20); // the value spaces out the children (aka the buttons)
+        vboxOpenScene.getChildren().addAll(l1, logInButton, registerButton); // add( ) adds in one button. addAll( ) adds all buttons.
+        openScene = new Scene(vboxOpenScene, 400, 400);
 
 
         //Layout 2 = StackPane - stacks children on in front (or on top) of each other
+
 
         //the register scene
         StackPane stackRegister = new StackPane();
         stackRegister.getChildren().addAll(l2, leaveRegister);
         registerScene = new Scene(stackRegister,400, 400);
 
+
         //the log in scene
         StackPane stackLogIn = new StackPane();
         stackLogIn.getChildren().addAll(l3, leaveLogIn);
         logInScene = new Scene(stackLogIn,400, 400);
 
-                //finally show your app
+
+        //finaly show your app
         window.setScene(openScene);
         window.setTitle("CME Bookings");
         window.show();
 
+
     }
+
 
     public static void main(String[] args) {
         launch(args);
     }
+
 
 }
