f6c0144b6b4601f0ae301c2d84c705fb4af8219d
ezra5 <ezra54311@gmail.com>
2024-12-02 19:14:54 -0500

    input reading wip

--- src/main/java/org/example/flightbooking/Admin.java
+++ src/main/java/org/example/flightbooking/Admin.java
@@ -2,7 +2,7 @@ package org.example.flightbooking;
 
 public class Admin extends Account{
     public Admin (String firstName, String lastName, String street, String zipCode, String state, String username,
-                     String password, String email, long SSN, String secQuestion, String secAnswer, String currentFlight, int secPIN) {
-        super(firstName, lastName, street, zipCode, state, username, password, email, SSN, secQuestion, secAnswer, currentFlight, secPIN);
+                     String password, String email, long SSN, String secQuestion, String secAnswer, int secPIN) {
+        super(firstName, lastName, street, zipCode, state, username, password, email, SSN, secQuestion, secAnswer, secPIN);
     }
 }
