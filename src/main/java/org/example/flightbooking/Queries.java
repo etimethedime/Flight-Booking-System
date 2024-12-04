1c71b0faa5b90ed6954e097add36773560a53019
ezra5 <ezra54311@gmail.com>
2024-12-04 14:40:55 -0500

    Queries Class created to list all needed queries

--- src/main/java/org/example/flightbooking/Queries.java
+++ src/main/java/org/example/flightbooking/Queries.java
@@ -2,12 +2,13 @@ package org.example.flightbooking;
 
 public class Queries {
 
+    public static final String REGISTER = ""; // INSERT
     public static final String LOGIN = ""; // SELECT
     public static final String FORGOTPASSWORD = ""; //SELECT
-    public static final String CHANGEPASSWORD = "";
-    public static final String BOOKFLIGHT = "";
-    public static final String CHANGEFLIGHT = "";
-    public static final String DELETEFLIGHT = "";
+    public static final String CHANGEPASSWORD = ""; // UPDATE
+    public static final String BOOKFLIGHT = ""; // INSERT
+    public static final String CHANGEFLIGHT = ""; // UPDATE
+    public static final String DELETEFLIGHT = ""; //DELETE
 
 
 
