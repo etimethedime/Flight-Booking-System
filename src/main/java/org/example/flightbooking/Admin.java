36a311fbef14e4e0d12653c145e7c30c54a4cb61
ezra5 <ezra54311@gmail.com>
2024-12-04 14:50:29 -0500

    Queries Class created to list all needed queries

--- src/main/java/org/example/flightbooking/Admin.java
+++ src/main/java/org/example/flightbooking/Admin.java
@@ -1,6 +1,6 @@
 package org.example.flightbooking;
 
-public class Admin extends Account{
+public class Admin extends Account {
     public Admin (String firstName, String lastName, String street, String zipCode, String state, String username,
                      String password, String email, String SSN, String secQuestion, String secAnswer, String secPIN) {
         super(firstName, lastName, street, zipCode, state, username, password, email, SSN, secQuestion, secAnswer, secPIN);
--- src/main/java/org/example/flightbooking/Queries.java
+++ src/main/java/org/example/flightbooking/Queries.java
@@ -2,13 +2,20 @@ package org.example.flightbooking;
 
 public class Queries {
 
+                // All for users
     public static final String REGISTER = ""; // INSERT
     public static final String LOGIN = ""; // SELECT
     public static final String FORGOTPASSWORD = ""; //SELECT
     public static final String CHANGEPASSWORD = ""; // UPDATE
+
+
+
+                // For Flights
+    public static final String GETFLIGHT = ""; // SELECT
     public static final String BOOKFLIGHT = ""; // INSERT
     public static final String CHANGEFLIGHT = ""; // UPDATE
     public static final String DELETEFLIGHT = ""; //DELETE
+
 
 
 
