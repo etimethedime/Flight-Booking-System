a6f395aeeec43294c193dc9f2b08705f1efb9004
mikiyasmekasha <mikiyasmekasha12@gmail.com>
2024-12-06 01:52:38 -0500

    added prepared statement for FORGOTPASSWORD

--- src/main/java/org/example/flightbooking/Queries.java
+++ src/main/java/org/example/flightbooking/Queries.java
@@ -6,7 +6,7 @@ public class Queries {
     public static final String REGISTER = "INSERT INTO User (Username, Password, FirstName, LastName, Email, PhoneNumber, Street, Zipcode, State, City, SSN, SecurityQuestion, SecurityAnswer)"  +
                         "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?); ";// INSERT
     public static final String LOGIN = "SELECT * FROM User WHERE Username = ? AND Password = ?"; // SELECT
-    public static final String FORGOTPASSWORD = ""; //SELECT
+    public static final String FORGOTPASSWORD = "SELECT Password FROM User WHERE Username = ? AND SecurityQuestion = ? AND SecurityAnswer = ?"; //SELECT
     public static final String CHANGEPASSWORD = ""; // UPDATE
 
 
