97246475136ea02c7296df04b4c1e8ddd2ab26bf
mikiyasmekasha <mikiyasmekasha12@gmail.com>
2024-12-06 17:22:10 -0500

    added prepared statement for DELETEFLIGHT

--- src/main/java/org/example/flightbooking/Queries.java
+++ src/main/java/org/example/flightbooking/Queries.java
@@ -15,7 +15,7 @@ public class Queries {
     public static final String GETFLIGHT = " SELECT Flight_ID, Flight_Number, Departure_City, Arrival_City, Departure_Time, Arrival_Time, Terminal FROM FLIGHTS WHERE Flight_ID = ?"; // SELECT
     public static final String BOOKFLIGHT = ""; // INSERT
     public static final String CHANGEFLIGHT = "UPDATE FLIGHTS SET Departure_Time = ?, Arrival_Time = ?, Terminal = ? WHERE Flight_ID = ?"; // UPDATE
-    public static final String DELETEFLIGHT = ""; //DELETE
+    public static final String DELETEFLIGHT = "DELETE FROM FLIGHTS WHERE Flight_ID = ?"; //DELETE
 
 
 
