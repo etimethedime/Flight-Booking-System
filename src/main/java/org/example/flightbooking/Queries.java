1835fd53c2272399ab4514ef5629aff008be9335
mikiyasmekasha <mikiyasmekasha12@gmail.com>
2024-12-06 01:43:00 -0500

    added prepared statement for LOGIN

--- src/main/java/org/example/flightbooking/Queries.java
+++ src/main/java/org/example/flightbooking/Queries.java
@@ -5,7 +5,7 @@ public class Queries {
                 // All for users
     public static final String REGISTER = "INSERT INTO User (Username, Password, FirstName, LastName, Email, PhoneNumber, Street, Zipcode, State, City, SSN, SecurityQuestion, SecurityAnswer)"  +
                         "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?); ";// INSERT
-    public static final String LOGIN = ""; // SELECT
+    public static final String LOGIN = "SELECT * FROM User WHERE Username = ? AND Password = ?"; // SELECT
     public static final String FORGOTPASSWORD = ""; //SELECT
     public static final String CHANGEPASSWORD = ""; // UPDATE
 
