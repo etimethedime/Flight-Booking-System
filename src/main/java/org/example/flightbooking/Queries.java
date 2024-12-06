a464570a789f97d7503f96ae090a05c97c1bcc91
mikiyasmekasha <mikiyasmekasha12@gmail.com>
2024-12-06 01:37:34 -0500

    added prepared statement for register

--- src/main/java/org/example/flightbooking/Queries.java
+++ src/main/java/org/example/flightbooking/Queries.java
@@ -3,7 +3,8 @@ package org.example.flightbooking;
 public class Queries {
 
                 // All for users
-    public static final String REGISTER = ""; // INSERT
+    public static final String REGISTER = "INSERT INTO User (Username, Password, FirstName, LastName, Email, PhoneNumber, Street, Zipcode, State, City, SSN, SecurityQuestion, SecurityAnswer)"  +
+                        "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?); ";// INSERT
     public static final String LOGIN = ""; // SELECT
     public static final String FORGOTPASSWORD = ""; //SELECT
     public static final String CHANGEPASSWORD = ""; // UPDATE
