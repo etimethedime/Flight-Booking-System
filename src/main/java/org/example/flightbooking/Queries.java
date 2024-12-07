ff65cdd45151f6545cf61ae866d1cdc42524572e
mikiyasmekasha <mikiyasmekasha12@gmail.com>
2024-12-07 14:58:34 -0500

    fixed the prepared statement for LOGIN

--- src/main/java/org/example/flightbooking/Queries.java
+++ src/main/java/org/example/flightbooking/Queries.java
@@ -5,7 +5,7 @@ public class Queries {
                 // All for users
     public static final String REGISTER = "INSERT INTO User (Username, Password, FirstName, LastName, Email, PhoneNumber, Street, Zipcode, State, City, SSN, SecurityQuestion, SecurityAnswer)"  +
                         "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?); ";// INSERT
-    public static final String LOGIN = "SELECT * FROM User WHERE Username = ? AND Password = ?"; // SELECT
+    public static final String LOGIN = "SELECT Username, Password FROM User WHERE Username = ? AND Password = ?"; // SELECT
     public static final String FORGOTPASSWORD = "SELECT Password FROM User WHERE Username = ? AND SecurityQuestion = ? AND SecurityAnswer = ?"; //SELECT
     public static final String CHANGEPASSWORD = "UPDATE User SET Password = ? WHERE Username =  ?"; // UPDATE
 
