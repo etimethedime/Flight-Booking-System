1e8c2f1d29c9fffc101d99d0336ebd59f9a87c38
ezra5 <ezra54311@gmail.com>
2024-12-08 21:08:02 -0500

    Account Settings WIP

--- src/main/java/org/example/flightbooking/AccountSettingsSceneController.java
+++ src/main/java/org/example/flightbooking/AccountSettingsSceneController.java
@@ -1,8 +1,21 @@
 package org.example.flightbooking;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.util.Objects;
+
 
 public class AccountSettingsSceneController {
+
+    public Parent AvailableFLightsRoot;
+    public Scene AvailableFlightsScene;
+    public Stage window;
+
     public void handleChangePassword(ActionEvent event) {
     }
 
@@ -10,5 +23,13 @@ public class AccountSettingsSceneController {
     }
 
     public void handleUpdateInformation(ActionEvent event) {
+    }
+
+    public void handleGoBackToMainPage(ActionEvent event) throws Exception {
+        AvailableFLightsRoot = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("AvaialableFlightsScene.fxml")));
+        window = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        AvailableFlightsScene = new Scene(AvailableFLightsRoot);
+        window.setScene(AvailableFlightsScene);
+        window.show();
     }
 }
