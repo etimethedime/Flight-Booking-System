3810388279182b25d7b1251c640c0e4923cb3014
cosmasobi <cosmasobi67@gmail.com>
2024-12-10 16:59:24 -0500

    made it so you access the admin register scene from the customer register scene

--- src/main/java/org/example/flightbooking/ControllerLogInScene.java
+++ src/main/java/org/example/flightbooking/ControllerLogInScene.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
