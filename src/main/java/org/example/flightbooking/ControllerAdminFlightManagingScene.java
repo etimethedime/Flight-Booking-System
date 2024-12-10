21e23023ffae272a7a48f5abc0d99870eda0fe49
mikiyasmekasha <mikiyasmekasha12@gmail.com>
2024-12-10 16:45:21 -0500

    set up variables for admin manage flights

--- src/main/java/org/example/flightbooking/ControllerAdminFlightManagingScene.java
+++ src/main/java/org/example/flightbooking/ControllerAdminFlightManagingScene.java
@@ -1,4 +1,70 @@
 package org.example.flightbooking;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class ControllerAdminFlightManagingScene {
+    @FXML
+    private TableView<Flight> tableView = new TableView<>();
+
+    @FXML
+    private TableColumn<Flight, String> FlightIDCol = new TableColumn<>();
+
+    @FXML
+    private TableColumn<Flight, String> FlightNumberCol = new TableColumn<>();
+    @FXML
+    private TableColumn<Flight, String> DepartureCityCol = new TableColumn<>();
+    @FXML
+    private TableColumn<Flight, String> ArrivalCityCol = new TableColumn<>();
+    @FXML
+    private TableColumn<Flight, String> DepartureTimeCol = new TableColumn<>();
+    @FXML
+    private TableColumn<Flight, String> ArrivalTimeCol = new TableColumn<>();
+    @FXML
+    private TableColumn<Flight, String> TerminalCol = new TableColumn<>();
+
+    @FXML
+    private ComboBox<String> seatOptionComboBox;
+
+
+    public void initialize() throws SQLException {
+        initializeFlightViewTable();
+
+        // Create the options for the ComboBox
+        ObservableList<String> seatOptions = FXCollections.observableArrayList("Aisle", "Window", "Middle");
+
+        // Set the items in the ComboBox
+        seatOptionComboBox.setItems(seatOptions);
+    }
+
+    public void initializeFlightViewTable() throws SQLException {
+        ObservableList<Flight> list = ControllerLogInScene.c1.getAllFlights();
+        FlightIDCol.setCellValueFactory(new PropertyValueFactory<>("FlightID"));
+        FlightNumberCol.setCellValueFactory(new PropertyValueFactory<>("FlightNO"));
+        DepartureCityCol.setCellValueFactory(new PropertyValueFactory<>("DepartureCity"));
+        ArrivalCityCol.setCellValueFactory(new PropertyValueFactory<>("ArrivalCity"));
+        DepartureTimeCol.setCellValueFactory(new PropertyValueFactory<>("DepartureTime"));
+        ArrivalTimeCol.setCellValueFactory(new PropertyValueFactory<>("ArrivalTime"));
+        TerminalCol.setCellValueFactory(new PropertyValueFactory<>("Terminal"));
+
+        tableView.setItems(list);
+        tableView.refresh();
+        System.out.println(list.toString());
+    }
 }
